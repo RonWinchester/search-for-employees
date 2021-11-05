815920fcd0b2f5c614264e39578493f4aae2e0c6
Ron Winchester <RonWinchester@yandex.ru>
2021-11-05 15:05:04 +0300

    feat: функционал закрытия и открытия popup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,45 @@
 import React from "react";
 import NavigationBar from "../NavigationBar/NavigationBar";
 import { Switch } from "react-router-dom";
+import Popup from "../Popup/Popup";
 
 function App() {
+  const [popupOpen, setPopupOpen] = React.useState(false);
+
+
+  //Работа Popup
+  function closeAllPopups() {
+    setPopupOpen(false);
+  }
+
+  function handleOverlayClose(evt) {
+    if (evt.target === evt.currentTarget) {
+      closeAllPopups();
+    }
+  }
+
+  React.useEffect(() => {
+    function handleEscClose(evt) {
+      if (evt.key === "Escape") {
+        closeAllPopups();
+      }
+    }
+    document.addEventListener("keyup", handleEscClose);
+
+    return () => {
+      document.removeEventListener("keyup", handleEscClose);
+    };
+  });
   return (
     <div className="page__container">
-      <NavigationBar></NavigationBar>
+      <NavigationBar setPopupOpen={setPopupOpen}></NavigationBar>
       <Switch></Switch>
+
+      <Popup
+          handleOverlayClose={handleOverlayClose}
+          isOpen={popupOpen}
+          onClose={closeAllPopups}
+        ></Popup>
     </div>
   );
 }
