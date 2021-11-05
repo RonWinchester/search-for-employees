c630a161566f89d0c8f4aaea43b6c4607194e974
Ron Winchester <RonWinchester@yandex.ru>
2021-11-05 18:19:28 +0300

    feat: компонент содержимого страницы

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EmploeesList from "../EmploeesList/EmploeesList";
+
+function Main () {
+    return (
+        <main className='main'>
+            <EmploeesList/>
+        </main>
+    )
+}
+
+export default Main;
