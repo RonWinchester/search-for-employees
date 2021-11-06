a3e5e1a03c64f20e8bae9b17f57e38929f515ea9
Ron Winchester <RonWinchester@yandex.ru>
2021-11-06 15:50:56 +0300

    fix: изменение отображения критической ошибки

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,14 +2,20 @@ import React from "react";
 import CriticalError from "../CriticalError/CriticalError";
 import EmploeesList from "../EmploeesList/EmploeesList";
 
-function Main () {
-const [error, setError] = React.useState(true)
+function Main() {
+  const [error, setError] = React.useState(false);
 
-    return (
-        <main className='main'>
-            {error? <CriticalError/>:<EmploeesList/>}
+  return (
+    <>
+      {error ? (
+        <CriticalError />
+      ) : (
+        <main className="main">
+          <EmploeesList />
         </main>
-    )
+      )}
+    </>
+  );
 }
 
 export default Main;
