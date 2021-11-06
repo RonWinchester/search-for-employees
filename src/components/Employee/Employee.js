36872755402115f2a93ce3f20c6c1a9cb5841b87
Ron Winchester <RonWinchester@yandex.ru>
2021-11-06 22:07:21 +0300

    fix: рефактор условий отображения прелоадера

--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -1,22 +1,16 @@
 import React from "react";
 
-function Emploee({ name, ini, pro, img }) {
-  const [preloader, setPreloader] = React.useState(true);
-
+function Emploee({
+  firstName,
+  lastName,
+  userTag,
+  position,
+  avatarUrl,
+  preloader,
+}) {
   return (
     <li className="emploee">
       {preloader ? (
-        <>
-          <img src={img} alt="" className="emploee__image" />
-          <dl className="emploee__info">
-            <dd className="emploee__name">
-              {name}
-              <span className="emploee__initials">{ini}</span>
-            </dd>
-            <dt className="emploee__specialization">{pro}</dt>
-          </dl>
-        </>
-      ) : (
         <>
           <div className="emploee__image emploee__preload"></div>
           <dl className="emploee__info">
@@ -24,6 +18,17 @@ function Emploee({ name, ini, pro, img }) {
               <span className="emploee__initials"></span>
             </dd>
             <dt className="emploee__specialization emploee__preload emploee__preload_specialization"></dt>
+          </dl>
+        </>
+      ) : (
+        <>
+          <img src={avatarUrl} alt="" className="emploee__image" />
+          <dl className="emploee__info">
+            <dd className="emploee__name">
+              {`${firstName} ${lastName}`}
+              <span className="emploee__initials">{userTag}</span>
+            </dd>
+            <dt className="emploee__specialization">{position}</dt>
           </dl>
         </>
       )}
