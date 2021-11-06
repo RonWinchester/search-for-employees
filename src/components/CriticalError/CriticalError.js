ba6a0519bb1b39b3aa54a00202d77081bdcfb9f4
Ron Winchester <RonWinchester@yandex.ru>
2021-11-06 15:16:12 +0300

    feat: компонент критической ошибки

---
+++ src/components/CriticalError/CriticalError.js
@@ -0,0 +1,13 @@
+import React from "react";
+import image from '../../images/critical-error-image.png'
+
+function CriticalError() {
+  return <div className='critical-error'>
+      <img src={image} alt='Критическая ошибка' className='critical-error__image'></img>
+      <h1 className='critical-error__title'>Какой-то сверхразум все сломал</h1>
+      <p className='critical-error__description'>Постараемся быстро починить</p>
+      <button className='critical-error__button'>Попробовать снова</button>
+  </div>;
+}
+
+export default CriticalError;
