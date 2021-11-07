0290621c70d897d1f2c9f723988aafb584631e42
Ron Winchester <RonWinchester@yandex.ru>
2021-11-07 22:26:06 +0300

    fix: добавление ссылки, рефакторинг пропсов

--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -1,37 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Emploee({
-  firstName,
-  lastName,
-  userTag,
-  position,
-  avatarUrl,
-  preloader,
-}) {
+function Emploee(props) {
+
+  const { firstName, lastName, userTag, position, avatarUrl, preloader, id, getEmploye } =  props;
+
+  function setEmploye () {
+    getEmploye(props);
+  };
+
   return (
     <li className="emploee">
-      {preloader ? (
-        <>
-          <div className="emploee__image emploee__preload"></div>
-          <dl className="emploee__info">
-            <dd className="emploee__name emploee__preload emploee__preload_name">
-              <span className="emploee__initials"></span>
-            </dd>
-            <dt className="emploee__specialization emploee__preload emploee__preload_specialization"></dt>
-          </dl>
-        </>
-      ) : (
-        <>
-          <img src={avatarUrl} alt="" className="emploee__image" />
-          <dl className="emploee__info">
-            <dd className="emploee__name">
-              {`${firstName} ${lastName}`}
-              <span className="emploee__initials">{userTag}</span>
-            </dd>
-            <dt className="emploee__specialization">{position}</dt>
-          </dl>
-        </>
-      )}
+      <Link className="emploee__link" to={`${id}`} onClick={()=>{setEmploye()}}>
+        {preloader ? (
+          <>
+            <div className="emploee__image emploee__preload"></div>
+            <dl className="emploee__info">
+              <dd className="emploee__name emploee__preload emploee__preload_name">
+                <span className="emploee__initials"></span>
+              </dd>
+              <dt className="emploee__specialization emploee__preload emploee__preload_specialization"></dt>
+            </dl>
+          </>
+        ) : (
+          <>
+            <img src={avatarUrl} alt="" className="emploee__image" />
+            <dl className="emploee__info">
+              <dd className="emploee__name">
+                {`${firstName} ${lastName}`}
+                <span className="emploee__initials">{userTag}</span>
+              </dd>
+              <dt className="emploee__specialization">{position}</dt>
+            </dl>
+          </>
+        )}
+      </Link>
     </li>
   );
 }
