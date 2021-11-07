9b7961137fe8ffc86f908da78a27d9e29380ef55
Ron Winchester <RonWinchester@yandex.ru>
2021-11-07 12:45:06 +0300

    feat: переключатель для ссылок

--- src/components/NavigationMenu/NavigationMenu.js
+++ src/components/NavigationMenu/NavigationMenu.js
@@ -1,15 +1,65 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function NavigationMenu() {
+  const location = useLocation();
+
   return (
     <nav className="navigation-menu">
-      <Link className="navigation-menu__link navigation-menu__link_active" to="/">Все</Link>
-      <Link className="navigation-menu__link" to="/designer">Designers</Link>
-      <Link className="navigation-menu__link" to="/analysts">Analysts</Link>
-      <Link className="navigation-menu__link" to="/managers">Managers</Link>
-      <Link className="navigation-menu__link" to="/ios">iOS</Link>
-      <Link className="navigation-menu__link" to="/android">Android</Link>
+      <Link
+        className={`navigation-menu__link ${
+          location.pathname === "/" ? "navigation-menu__link_active" : ""
+        }`}
+        to="/"
+      >
+        Все
+      </Link>
+      <Link
+        className={`navigation-menu__link ${
+          location.pathname === "/designer"
+            ? "navigation-menu__link_active"
+            : ""
+        }`}
+        to="/designer"
+      >
+        Designers
+      </Link>
+      <Link
+        className={`navigation-menu__link ${
+          location.pathname === "/analysts"
+            ? "navigation-menu__link_active"
+            : ""
+        }`}
+        to="/analysts"
+      >
+        Analysts
+      </Link>
+      <Link
+        className={`navigation-menu__link ${
+          location.pathname === "/managers"
+            ? "navigation-menu__link_active"
+            : ""
+        }`}
+        to="/managers"
+      >
+        Managers
+      </Link>
+      <Link
+        className={`navigation-menu__link ${
+          location.pathname === "/ios" ? "navigation-menu__link_active" : ""
+        }`}
+        to="/ios"
+      >
+        iOS
+      </Link>
+      <Link
+        className={`navigation-menu__link ${
+          location.pathname === "/android" ? "navigation-menu__link_active" : ""
+        }`}
+        to="/android"
+      >
+        Android
+      </Link>
     </nav>
   );
 }
