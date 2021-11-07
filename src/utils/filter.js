064177ce8d842da6ff7625104bc1b2c308707cf7
Ron Winchester <RonWinchester@yandex.ru>
2021-11-07 19:30:07 +0300

    fix: доработка фильтра

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,7 +1,21 @@
-export function handleSelectionProfession (employees, profession) {
-    let result = [];
-    employees.forEach(employee => {
-        employee.department === profession && result.push(employee)
-    })
-    return result
+export function handleSelectionProfession(employees, profession) {
+  let result = [];
+  employees.forEach((employee) => {
+    employee.department === profession && result.push(employee);
+  });
+  return result;
+}
+
+export function handleFilter(employees, query) {
+  let result = [];
+  if (query.length !== 0) {
+    result = employees.filter((employee) => {
+      return employee.firstName.toLowerCase().includes(query.toLowerCase()) ||
+      employee.lastName.toLowerCase().includes(query.toLowerCase()) ||
+      employee.userTag.toLowerCase().includes(query.toLowerCase());
+    });
+  } else {
+    result = employees;
   }
+  return result;
+}
