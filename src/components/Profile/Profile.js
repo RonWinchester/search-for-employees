7f1f70149c281cab6662ba97a3edba380a2f448f
Ron Winchester <RonWinchester@yandex.ru>
2021-11-08 23:10:30 +0300

    fix: верстка страницы профиля

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,24 +3,54 @@ import { useHistory } from "react-router-dom";
 
 function Profile(props) {
   const history = useHistory();
-  const { firstName, lastName, userTag, position, avatarUrl, birthday, phone } =  props.employee;
+  const { firstName, lastName, userTag, position, avatarUrl, birthday, phone } =
+    props.employee;
+
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const birthdate = new Date(birthday).toLocaleString("ru", options);
+  const years = +new Date().getFullYear() - new Date(birthday).getFullYear();
 
   return (
     <div className="profile">
       <div className="profile__info">
-        <button className='profile__button' onClick={() => history.goBack()}></button>
-        <article className='profile__bio'>
-          <img src={avatarUrl} alt={`${firstName} ${lastName}`} className='profile__image'></img>
-          <h1 className='profile__title'>
-          {`${firstName} ${lastName}`}<span className='profile__initials'>{userTag}</span>
+        <button
+          className="profile__button"
+          onClick={() => history.goBack()}
+        ></button>
+        <article className="profile__bio">
+          <img
+            src={avatarUrl}
+            alt={`${firstName} ${lastName}`}
+            className="profile__image"
+          ></img>
+          <h1 className="profile__title">
+            {`${firstName} ${lastName}`}
+            <span className="profile__initials">{userTag}</span>
           </h1>
-          <p className='profile__specialization'>{position}</p>
+          <p className="profile__specialization">{position}</p>
         </article>
       </div>
       <div className="profile__contact">
-        <p className='profile__date'>{birthday}</p>
-        <span className='profile__years'>24 года</span>
-        <a href="tel" className='profile__tel'>{phone}</a>
+        <p className="profile__date">
+          {birthdate.slice(0, birthdate.length - 2)}
+        </p>
+        <span className="profile__years">
+          {String(years).endsWith("1")
+            ? `${years} год`
+            : String(years).endsWith("2") |
+              String(years).endsWith("3") |
+              String(years).endsWith("4")
+            ? `${years} года`
+            : `${years} лет`}
+        </span>
+        <a href="tel" className="profile__tel">
+          {phone}
+        </a>
       </div>
     </div>
   );
