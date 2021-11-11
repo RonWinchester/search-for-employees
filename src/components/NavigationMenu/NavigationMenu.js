a984a525c1bd11402a1adf1210aac2995fff4407
Ron Winchester <RonWinchester@yandex.ru>
2021-11-11 23:00:06 +0300

    fix: вывод ссылок

--- src/components/NavigationMenu/NavigationMenu.js
+++ src/components/NavigationMenu/NavigationMenu.js
@@ -1,65 +1,22 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { links } from "../../constants/constants";
 
 function NavigationMenu() {
   const location = useLocation();
 
   return (
     <nav className="navigation-menu">
-      <Link
-        className={`navigation-menu__link ${
-          location.pathname === "/" ? "navigation-menu__link_active" : ""
-        }`}
-        to="/"
-      >
-        Все
-      </Link>
-      <Link
-        className={`navigation-menu__link ${
-          location.pathname === "/designer"
-            ? "navigation-menu__link_active"
-            : ""
-        }`}
-        to="/designer"
-      >
-        Designers
-      </Link>
-      <Link
-        className={`navigation-menu__link ${
-          location.pathname === "/analysts"
-            ? "navigation-menu__link_active"
-            : ""
-        }`}
-        to="/analysts"
-      >
-        Analysts
-      </Link>
-      <Link
-        className={`navigation-menu__link ${
-          location.pathname === "/managers"
-            ? "navigation-menu__link_active"
-            : ""
-        }`}
-        to="/managers"
-      >
-        Managers
-      </Link>
-      <Link
-        className={`navigation-menu__link ${
-          location.pathname === "/ios" ? "navigation-menu__link_active" : ""
-        }`}
-        to="/ios"
-      >
-        iOS
-      </Link>
-      <Link
-        className={`navigation-menu__link ${
-          location.pathname === "/android" ? "navigation-menu__link_active" : ""
-        }`}
-        to="/android"
-      >
-        Android
-      </Link>
+      {links.map((link, index) => (
+        <Link key={index}
+          className={`navigation-menu__link ${
+            location.pathname === link.link ? "navigation-menu__link_active" : ""
+          }`}
+          to={link.link}
+        >
+          {link.name}
+        </Link>
+      ))}
     </nav>
   );
 }
