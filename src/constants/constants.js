423166c800d14a3cdf5d4c9b6fc130ab37261eda
Ron Winchester <RonWinchester@yandex.ru>
2021-11-11 22:59:02 +0300

    fix: добавлены все департаменты в общий массив

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,30 @@
 export const department = {
-    design: 'design',
-    android: 'android',
-    ios: 'ios',
-    management: 'management',
-    analytics: 'analytics',
-}
+  design: "design",
+  android: "android",
+  ios: "ios",
+  management: "management",
+  analytics: "analytics",
+  qa: "qa",
+  back_office: "back_office",
+  frontend: "frontend",
+  hr: "hr",
+  pr: "pr",
+  backend: "backend",
+  support: "support",
+};
+
+export const links = [
+  { link: "/", name: "Все" },
+  { link: "/designer", name: "Дизайн" },
+  { link: "/analysts", name: "Аналитика" },
+  { link: "/managers", name: "Менеджмент" },
+  { link: "/ios", name: "iOS" },
+  { link: "/android", name: "Android" },
+  { link: "/qa", name: "QA" },
+  { link: "/back_office", name: "Бэк-офис" },
+  { link: "/hr", name: "HR" },
+  { link: "/pr", name: "PR" },
+  { link: "/backend", name: "Backend" },
+  { link: "/support", name: "Техподдержка" },
+  { link: "/frontend", name: "Frontend" }
+];
